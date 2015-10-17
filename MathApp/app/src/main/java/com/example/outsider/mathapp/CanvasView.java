72cd21bbd86af5b71f36b4f4f19a9719f4756d99
Brandon Stanley <bstanley@lakeheadu.ca>
2015-10-17 03:01:30 -0400

    Geometry
    Did some geometric shape drawing, with dynamic binded elements for the
    sake of showing how to do it.

--- MathApp/app/src/main/java/com/example/outsider/mathapp/CanvasView.java
+++ MathApp/app/src/main/java/com/example/outsider/mathapp/CanvasView.java
@@ -6,6 +6,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +16,14 @@ import android.view.ViewGroup;
  * Created by Outsider on 10/5/2015.
  */
 public class CanvasView extends View {
-    private int width, height;
     private Context context;
     private Path path;
     private Bitmap bitmap;
     private Canvas canvas;
     private Paint paint;
+
+    private String shape;
+    private double width, height;
 
     public CanvasView(Context c, AttributeSet att){
         super(c, att);
@@ -34,26 +38,51 @@ public class CanvasView extends View {
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setStrokeWidth(4f);
-
+        shape = "None";
 
         invalidate();
 
+    }
+
+    public void setShape(String shape, double width, double height){
+        this.shape = shape;
+        this.width = width;
+        this.height = height;
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh){
         super.onSizeChanged(w, h, oldw, oldh);
 
-        bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         canvas = new Canvas(bitmap);
     }
 
     @Override
     protected void onDraw(Canvas canvas){
-        super.onDraw(canvas);
+        //canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+        switch(shape) {
+            case "Rectangle":
+                canvas.drawRect(10, 10, (float)(50 * width), (float)(50 * height), paint);
+                break;
+            case "Ellipse":
+                canvas.drawOval(new RectF(10, 10, (float)(50 * width), (float)(50 * height)), paint);
+                break;
+            case "Triangle":
+                Path path = new Path();
+                path.moveTo(10,10);
+                path.lineTo(10, (float)(50 * height));
+                path.moveTo(10, (float)(50 * height));
+                path.lineTo((float)(50 * width), (float)(50 * height));
+                path.moveTo((float)(50 * width), (float)(50 * height));
+                path.lineTo(10,10);
+                path.close();
+                canvas.drawPath(path, paint);
+                break;
+            default:
 
-        canvas.drawRect(10, 10, 500, 500, paint);
-
+                break;
+        }
     }
     @Override
     protected void onMeasure( int widthMeasureSpec, int heightMeasureSpec )
--- MathApp/app/src/main/java/com/example/outsider/mathapp/geometry.java
+++ MathApp/app/src/main/java/com/example/outsider/mathapp/geometry.java
@@ -2,17 +2,27 @@ package com.example.outsider.mathapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import static android.widget.AdapterView.*;
 
-public class geometry extends AppCompatActivity {
+public class geometry extends AppCompatActivity
+        implements OnItemSelectedListener, OnClickListener{
+    CanvasView canvas;
+
+    EditText width, height;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +33,33 @@ public class geometry extends AppCompatActivity {
         String [] options = { "Rectangle", "Ellipse", "Triangle"};
         geometricSelection.setAdapter(new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, options));
         geometricSelection.setSelection(0);
-        geometricSelection.setOnItemSelectedListener(new itemSelect());
+        geometricSelection.setOnItemSelectedListener(this);
 
-        //ImageView canvas = (ImageView)findViewById(R.id.geometricCanvas);
-        //canvas.setAdjustViewBounds(true);
+        Button submit = (Button)findViewById(R.id.geometricSubmit);
+        submit.setOnClickListener(this);
 
+        canvas = (CanvasView)findViewById(R.id.geoCanvas);
+
+        LinearLayout layout = (LinearLayout)findViewById(R.id.geoInputs);
+
+        TextView widthLbl = new TextView(this);
+        widthLbl.setText("Width: ");
+        layout.addView(widthLbl);
+
+        width = new EditText(this);
+        width.setWidth(150);
+        width.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        layout.addView(width);
+
+        TextView heightLbl = new TextView(this);
+        heightLbl.setText(" Height: ");
+        layout.addView(heightLbl);
+
+        height = new EditText(this);
+        height.setWidth(150);
+        height.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        layout.addView(height);
     }
 
     @Override
@@ -52,16 +84,24 @@ public class geometry extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    protected class itemSelect implements OnItemSelectedListener{
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    }
 
-        }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
+    }
 
-        }
+    @Override
+    public void onClick(View v) {
+        Spinner menu = (Spinner)findViewById(R.id.geometricShape);
+
+        Double Width = Double.parseDouble(width.getText().toString());
+        Double Height = Double.parseDouble(height.getText().toString());
+
+        canvas.setShape(menu.getSelectedItem().toString(), Width, Height);
+        canvas.invalidate();
     }
 }
